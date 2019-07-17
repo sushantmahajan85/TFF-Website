ff03948c96f51c6466bbeed9bc39e2059d7928fc
sushantmahajan85 <sushantmahajan85@gmail.com>
2019-07-17 22:44:30 +0530

    16th july

---
+++ tffjs.js
@@ -0,0 +1,9 @@
+
+var opa = document.getElementById("navigation_back");
+var link = document.getElementsByClassName("navigation_link");
+ 
+link.addEventListener("click",function(){
+
+alert("hello");
+
+});
